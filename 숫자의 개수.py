cf31b0e29015d4705764c770f08ed5a3cbaad666
WooriHCho <woori.helena.cho@gmail.com>
2022-01-15 18:05:59 +0900

    2577 숫자의 개수

---
+++ 숫자의 개수.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+total = a*b*c
+rslt = list(str(total))
+
+print(rslt.count('0'))
+print(rslt.count('1'))
+print(rslt.count('2'))
+print(rslt.count('3'))
+print(rslt.count('4'))
+print(rslt.count('5'))
+print(rslt.count('6'))
+print(rslt.count('7'))
+print(rslt.count('8'))
+print(rslt.count('9'))
