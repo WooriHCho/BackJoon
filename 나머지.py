7d1de01198ad564d574226977480a99ca5f48b34
WooriHCho <woori.helena.cho@gmail.com>
2022-05-14 19:14:43 +0900

    0514 정리

--- 나머지.py
+++ 나머지.py
@@ -1,13 +1,15 @@
-a = []
-cnt=0
+from re import A
 
-for i in range(0, 10, 1):
-    a.append(int(input()) % 42)
 
-for i in range(42):
-    if i in a:
-        cnt+=1
-    else:
-        pass
-    
-print(cnt)
+x = int(input())
+y = int(input())
+
+a = x * (y%10)
+b = x * ((y//10)%10)
+c = x * (y//100)
+d = a + b*10 + c*100
+
+print(a)
+print(b)
+print(c)
+print(d)
